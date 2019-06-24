efc4741e30cc375f3c8c73239710724c456ac6a3
Winston <winston@Winstons-MacBook-Pro.local>
2019-06-24 04:35:41 -0700

    erase no longer in use -thx

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-import pickle
 import pandas as pd
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template
 
 # app object to route calls
 app = Flask( __name__ )
@@ -23,28 +22,6 @@ def ds():
 @app.route('/media', methods=['GET'])
 def media():
     return render_template('media.html')
-
-'''unsure below'''
-# mpg route
-@app.route('/mpg', methods = ['GET'])
-def mpg():
-    return render_template('mpg.html')
-# load model
-model = pickle.load(open('linreg.p', 'rb'))
-# inference route
-@app.route('/inference',methods= ['POST'])
-def inference():
-    req = request.get_json()
-    print(req)
-    c,h,w = req['cylinders'], req['horsepower'], req['weight']
-    prediction = list(model.predict([[c,h,w]]))
-    return jsonify({'c':c, 'h':h, 'w':w, 'prediction':prediction[0]})
-# scatter plot
-@app.route('/plot', methods=['GET'])
-def plot():
-    df = pd.read_csv('cars.csv')
-    data = list(zip(df.mpg , df.weight))
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3333, debug=True)
